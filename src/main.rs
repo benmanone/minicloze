ab597a83a6b5921646496bbe1b4cbf8fa32910b1
thaumatrge <benjychada@icloud.com>
2023-02-04 09:31:50 +0000

    Ensure that each game has 9 sentences
    This will cause problems if a language with less than 9 sentences in total is passed to the game

--- src/main.rs
+++ src/main.rs
@@ -9,53 +9,89 @@ use unescaper;
 fn main() -> Result<(), minreq::Error> {
     let args: Vec<_>  = env::args().collect();
     let mut language: String;
+
     if args.len() > 1 {
         language = (&args[1]).to_string();
     }
     else {
         language = String::new();
+
         print!("What language do you want to study? ");
+
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut language).unwrap();
+
         language = language.trim().to_string();
     }
     print!("Fetching sentences for you...");
-    // allows output on the same line
     io::stdout().flush().unwrap();
+
     let now = Instant::now();
-    let request = "https://tatoeba.org/en/api_v0/search?from=eng&orphans=no&sort=random&to=".to_owned()+&language.trim()+"&unapproved=no";
-    let response = minreq::get(request).send()?;
-    let elapsed = now.elapsed();
-    let rep_string = response.as_str()?;
-    // finds the start of results by locating the first [ in rep_string
-    let results_start = 1+get_char_locations(rep_string, '[')[1];
-    let results = &rep_string[results_start..rep_string.len()-2];
-    let sentences = parse(results);
+
+    let sentences = generate_sentences(&language).unwrap();
     let len = sentences.len();
+
+    let elapsed = now.elapsed();
+
     print!(" Processing complete in {:.2?}, {} sentences parsed.\n", elapsed, len);
+
     play(sentences, len, language);
     Ok(())
+}
+
+fn http_request(language: &str) -> Result<Vec<Sentence>, minreq::Error> {
+    let request = "https://tatoeba.org/en/api_v0/search?from=eng&orphans=no&sort=random&to=".to_owned()+language+"&unapproved=no";
+    let response = minreq::get(request).send()?;
+
+    let rep_string = response.as_str()?;
+
+    let results_start = 1+get_char_locations(rep_string, '[')[1];
+
+    let results = &rep_string[results_start..rep_string.len()-2];
+    let sentences = parse(results);
+    Ok(sentences)
+}
+
+fn generate_sentences(language: &str) -> Result<Vec<Sentence>, minreq::Error> {
+    let mut sentences = http_request(language).unwrap();
+    let len = sentences.len();
+
+    if len != 9 {
+        let difference = 9 - len;
+        let mut sentences_difference = http_request(language).unwrap().into_iter().take(difference).collect::<Vec<_>>();
+
+        sentences.append(&mut sentences_difference);
+    }
+    Ok(sentences)
 }
 
 fn play(sentences: Vec<Sentence>, len: usize, language: String) {
     let mut correct = 0;
     let non_spaced = ["cmn", "lzh", "hak", "cjy", "nan", "hsn", "gan", "jpn", "tha", "khm", "lao", "mya"];
+
     for sentence in sentences {
         let mut cropped = sentence.translation;
         cropped.pop();
+
         println!();
+
         let rand = &mut rand::thread_rng();
         let words: Vec<String>;
+
         let is_non_spaced = non_spaced.iter().any(|x| x == &language);
+
         if is_non_spaced {
             words = cropped.chars().map(|x| x.to_string()).collect::<Vec<String>>();
         }
         else {
             words = cropped.split(' ').map(|x| x.to_string()).collect::<Vec<String>>();
         }
+
         let mut shuffled = words.clone();
         shuffled.shuffle(rand);
+
         let word = &shuffled[0];
+
         for i in words {
             if &i.to_string() == word {
                 for _j in word.chars() {
@@ -72,7 +108,9 @@ fn play(sentences: Vec<Sentence>, len: usize, language: String) {
                 print!("{}", i);
             }
         }
+
         println!("\n{}", sentence.text);
+
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).unwrap();
         if guess.trim().to_lowercase().contains(&word.to_lowercase()) {
@@ -94,11 +132,16 @@ fn parse(results: &str) -> Vec<Sentence> {
     // sentence_and_remainder yields both halves of results split at 2nd },
     for _i in 1..10 {
         let sentence_and_remainder = raw.match_indices("},{").nth(0).map(|(index, _)| raw.split_at(index));
+
         if let Some(s) = sentence_and_remainder {
             let including_bracket = (s.0).to_owned() + "}";
             let sentence = Sentence::new(&including_bracket);
+
             if sentence.id != -1 {
                 sentences.push(sentence);
+            }
+            else {
+                print!(" Sentence rejected...");
             }
             raw = &s.1[2..];
         }
@@ -131,7 +174,7 @@ impl Sentence {
             let translation_end = x+translation_start;
             Sentence {
                 id: string[id_position+4..(string[id_position-2..].find(',').unwrap())].parse::<i32>().unwrap(),
-                text: string[text_start..text_end-1].to_string(),
+                text: parse_unicode(&string[text_start..text_end-1].to_string()),
                 translation: parse_unicode(&string[translation_start..translation_end].to_string()),
             }
         }
@@ -144,13 +187,17 @@ impl Sentence {
 fn parse_unicode(string: &str) -> String {
     let mut i = 0;
     let mut chars: Vec<char> = Vec::new();
+
     while i < string.len() {
         if string.as_bytes()[i] as char == '\\' {
             let number = &string[i+2..i+6];
             let format = "\\".to_owned() + "u" + "{" + number + "}";
+
             let result = unescaper::unescape(format.as_str());
+
             if let Ok(x) = result {
                 let character = x.chars().next().unwrap();
+
                 chars.push(character);
             }
             i += 6;
